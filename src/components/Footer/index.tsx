b896fe8ce00f9c10b57df8fb3c86cf599e301ba5
kay <clarissa_kayano@hotmail.com>
2023-02-01 01:42:13 -0300

    Diminuindo o tamanho do Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 import { memo } from 'react';
 
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import {
   BsFillSuitHeartFill,
   BsGithub,
@@ -18,46 +18,53 @@ import { BgColor, By, Color } from './styles';
 const Footer: React.FC = () => (
   <BgColor>
     <Container className="mb-2">
-      <div className="d-flex justify-content-center mt-2">
-        <SiNetlify color="#ffffff" size={30} />
-        <div>
-          <div className="col">
-            <span className="color m-2">DEPLOYS BY</span>
+      <Row className="row row-cols-1 row-cols-sm-2 row-cols-md-3">
+        <Col>
+          <div className="d-flex justify-content-center mt-2">
+            <SiNetlify color="#ffffff" size={30} />
+            <div>
+              <div className="col">
+                <span className="color m-2">DEPLOYS BY</span>
+              </div>
+
+              <div>
+                <Color className=" m-2">netlify</Color>
+              </div>
+            </div>
           </div>
+        </Col>
+        <Col>
+          <div className="d-flex justify-content-center mt-3">
+            <div>
+              <a href="https://github.com/clarissakayano" target="blank">
+                <BsGithub className="me-3" color="#6f7072" />
+              </a>
+            </div>
+            <a href="https://twitter.com/i/flow/login" target="blank">
+              <BsTwitter className="me-3" color="#6f7072" />
+            </a>
 
-          <div className="col">
-            <Color className=" m-2">netlify</Color>
+            <a href="https://www.instagram.com/" target="blank">
+              <BsFillSuitHeartFill className="me-3" color="#6f7072" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/clarissa-a-kayano-37a011259/"
+              target="blank"
+            >
+              <BsLinkedin className="me-3" color="#6f7072" />
+            </a>
           </div>
-        </div>
-      </div>
-      <div className="d-flex justify-content-center mt-3">
-        <div>
-          <a href="https://github.com/clarissakayano" target="blank">
-            <BsGithub className="me-3" color="#6f7072" />
-          </a>
-        </div>
-        <a href="https://twitter.com/i/flow/login" target="blank">
-          <BsTwitter className="me-3" color="#6f7072" />
-        </a>
-
-        <a href="https://www.instagram.com/" target="blank">
-          <BsFillSuitHeartFill className="me-3" color="#6f7072" />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/clarissa-a-kayano-37a011259/"
-          target="blank"
-        >
-          <BsLinkedin className="me-3" color="#6f7072" />
-        </a>
-      </div>
-
-      <div className="d-flex justify-content-center mt-3">
-        <MdOutlineArrowBackIosNew color="#6f7072" />
-        <MdOutlineArrowForwardIos color="#6f7072" />
-        <By>
-          by <Color>Clarissa Kayano </Color>2022
-        </By>
-      </div>
+        </Col>
+        <Col>
+          <div className="d-flex justify-content-center mt-3">
+            <MdOutlineArrowBackIosNew color="#6f7072" />
+            <MdOutlineArrowForwardIos color="#6f7072" />
+            <By>
+              by <Color>Clarissa Kayano </Color>2022
+            </By>
+          </div>
+        </Col>
+      </Row>
     </Container>
   </BgColor>
 );
