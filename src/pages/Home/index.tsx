0e8a3779a4a0f66e7e98b79893774b1a9e032b66
kay <clarissa_kayano@hotmail.com>
2023-02-01 23:16:26 -0300

    Colocando title no Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,6 +6,8 @@ import CharacterCard from 'components/CharacterCard';
 import Footer from 'components/Footer';
 import Header from 'components/Header';
 
+import useTitle from 'hooks/useTitle';
+
 import { Pagination } from 'styles/GlobalStyles/Pagination';
 
 import { CharacterType } from 'types/CharacterType';
@@ -40,6 +42,13 @@ const Home: React.FC = () => {
     },
     [fetchCharacters],
   );
+
+  const setTitle = useTitle();
+
+  useEffect(() => {
+    setTitle('Home');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  });
 
   return (
     <>
